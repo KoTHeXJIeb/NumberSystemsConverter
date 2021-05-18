aff1553e405af0d597c79d7a0e44d1aac43e3de8
CatProgrammerYT <53112228+CatProgrammerYT@users.noreply.github.com>
2021-05-18 18:41:54 +0300

    Added main app class

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ class Main:
     def __init__(self) -> None:
         pass
 
-    def main():
-        pass
+    def main(self):
+        tk = tkinter.Tk()
+        tk.geometry("800x400")
+        tk.title('Number system converter')
+        tk.mainloop()
+
+main = Main()
+main.main()
